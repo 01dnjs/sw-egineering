aa3b909c430a86a8cdeffe4590fef8536c6b46ae
powdersnow0604 <gundam6324@gmail.com>
2025-03-21 23:04:56 +0900

    delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-data = [{"Word": "hello", "Meaning": "안녕", "Multiplicity":1},
-        {"Word": "world", "Meaning": "세계", "Multiplicity":5}]
-
-df = pd.DataFrame(data)
-
-print(df)
