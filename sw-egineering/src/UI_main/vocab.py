21eb0de6c0d2974212c083b7f2c25573aa418c3c
01dnjs <126755691+01dnjs@users.noreply.github.com>
2025-04-01 11:11:09 +0900

    Add files via upload

---
+++ sw-egineering/src/UI_main/vocab.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+
+def vocab_window(root):
+    for widget in root.winfo_children():  # 기존 UI 제거
+        widget.destroy()
+    root.title("단어장")
+    root.geometry("400x500")
+    
+    label = ttk.Label(root, text="단어장 화면", font=("Arial", 18, "bold"), bootstyle="primary")
+    label.pack(pady=20)
+    
+    exit_button = ttk.Button(root, text="닫기", bootstyle="danger", command=root.destroy)
+    exit_button.pack(pady=20)
+    
+    root.mainloop()
+    
+    
+
+if __name__ == "__main__":
+    vocab_window()
+    
